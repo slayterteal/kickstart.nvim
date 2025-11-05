1b650d13d468b8d3ebdb899e11ded1b95713b8a2
Slayter Teal <slayterteal@gmail.com>
2025-11-05 17:05:02 -0600

    trying to get javascript lsp working

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -1,3 +1,9 @@
+-- Helper function to make it easier to map certain keys
+local map = function(keys, func, desc, event)
+  vim.keymap.set('n', keys, func, { buffer = event.buf, desc = desc })
+end
+
+-- install lazy.nvim if needed
 function lazy_install_if_needed() 
   -- [[ Install `lazy.nvim` plugin manager ]]
   --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
@@ -12,7 +18,7 @@ function lazy_install_if_needed()
   vim.opt.rtp:prepend(lazypath)
 end
 
-
+-- lazy load all the plugins
 function lazy_load()
   -- [[ Configure and install plugins ]]
   -- NOTE: Here is plugins are setup!
@@ -203,7 +209,7 @@ function lazy_load()
         },
         { 'Bilal2453/luvit-meta', lazy = true },
       },
-      config = function()
+      config = function() 
         -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
         -- and elegantly composed help section, `:help lsp-vs-treesitter`
 
@@ -214,51 +220,42 @@ function lazy_load()
         vim.api.nvim_create_autocmd('LspAttach', {
           group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
           callback = function(event)
-            -- NOTE: Remember that Lua is a real programming language, and as such it is possible
-            -- to define small helper and utility functions so you don't have to repeat yourself.
-            --
-            -- In this case, we create a function that lets us more easily define mappings specific
-            -- for LSP related items. It sets the mode, buffer and description for us each time.
-            local map = function(keys, func, desc)
-              vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-            end
-
             -- Jump to the definition of the word under your cursor.
             --  This is where a variable was first declared, or where a function is defined, etc.
             --  To jump back, press <C-t>.
-            map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+            map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition', event)
 
             -- Find references for the word under your cursor.
-            map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+            map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences', event)
 
             -- Jump to the implementation of the word under your cursor.
             --  Useful when your language has ways of declaring types without an actual implementation.
-            map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+            map('gi', require('telescope.builtin').lsp_implementations, '[g]oto [i]mplementation', event)
 
             -- Jump to the type of the word under your cursor.
             --  Useful when you're not sure what type a variable is and you want to see
             --  the definition of its *type*, not where it was *defined*.
-            map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+            map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition', event)
 
             -- Fuzzy find all the symbols in your current document.
             --  Symbols are things like variables, functions, types, etc.
-            map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+            map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols', event)
 
             -- Fuzzy find all the symbols in your current workspace.
             --  Similar to document symbols, except searches over your entire project.
-            map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+            map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols', event)
 
             -- Rename the variable under your cursor.
             --  Most Language Servers support renaming across files, etc.
-            map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+            map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame', event)
 
             -- Execute a code action, usually your cursor needs to be on top of an error
             -- or a suggestion from your LSP for this to activate.
-            map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+            map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', event)
 
             -- WARN: This is not Goto Definition, this is Goto Declaration.
             --  For example, in C this would take you to the header.
-            map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+            map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration', event)
 
             -- The following two autocommands are used to highlight references of the
             -- word under your cursor when your cursor rests there for a little while.
@@ -328,12 +325,20 @@ function lazy_load()
           -- rust_analyzer = {},
           -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
           --
-          -- Some languages (like typescript) have entire language plugins that can be useful:
-          --    https://github.com/pmizio/typescript-tools.nvim
-          --
-          -- But for many setups, the LSP (`tsserver`) will work just fine
-          -- tsserver = {},
-          --
+          ts_ls = {
+            settings = {
+              typescript = {
+                preferences = {
+                  preferGoToSourceDefinition = true
+                }
+              },
+              javascript = {
+                preferences = {
+                  preferGoToSourceDefinition = true
+                }
+              }
+            }
+          },
 
           lua_ls = {
             -- cmd = {...},
@@ -364,6 +369,7 @@ function lazy_load()
         local ensure_installed = vim.tbl_keys(servers or {})
         vim.list_extend(ensure_installed, {
           'stylua', -- Used to format Lua code
+          'ts_ls',  -- typescript? I think it's also used for some other stuff...
         })
         require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -392,7 +398,6 @@ function lazy_load()
         }
       end,
     },
-
     { -- Autocompletion
       'hrsh7th/nvim-cmp',
       event = 'InsertEnter',
@@ -804,6 +809,25 @@ function lazy_load()
         suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
         -- log_level = 'debug',
       },
+    },
+    {
+      "hedyhli/outline.nvim",
+      event = 'VimEnter',
+      config = function()
+
+        vim.api.nvim_create_autocmd('LspAttach', {
+          group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+          callback = function(event)
+            -- Example mapping to toggle outline
+            -- vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>",
+            --   { desc = "Toggle Outline" })
+            map('<leader>oo', "<cmd>Outline!<CR>", '[O]pen [O]utline', event)
+          end,
+        })
+        require("outline").setup {
+          -- Your setup opts here (leave empty to use defaults)
+        }
+      end,
     },
 
     --  Here are some example plugins that I've included in the Kickstart repository.
