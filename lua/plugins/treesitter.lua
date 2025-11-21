7e30f8aaf1fff1e6471c91ecd48de5beae82902f
slayterteal <slayterteal@gmail.com>
2025-11-21 09:30:00 -0600

    tweaks for windows treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -17,4 +17,39 @@ return {
       -- add languages you need
     },
   },
+  highlight = {
+    enable = true,
+    -- disable = { "html" }, -- examples if a parser is unstable
+    additional_vim_regex_highlighting = false,
+  },
+
+  indent = {
+    enable = true,
+  },
+
+  -- optional playground (useful for debugging)
+  playground = {
+    enable = false,         -- set true if you want the playground UI
+    updatetime = 25,
+  },
+
+  matchup = {
+    enable = true, -- if you use romainl's vim-matchup
+  },
+
+  -- incremental selection (optional)
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
+
+  -- optional: friendly message after install (not required)
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
