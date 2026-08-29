fe62136ceb9006f0b40aceeec28a206d314aaf0f
slayterteal <slayterteal@gmail.com>
2026-08-29 11:02:24 -0500

    Trim whitespace on save and fix conform.nvim keybinds

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,26 +1,31 @@
 return {
   {
     'stevearc/conform.nvim',
+    event = { 'BufWritePre' }, -- lazy-load, but not for autoformatting
+    cmd = { 'ConformInfo' },
+    keys = {
+      -- Add a command to handle formatting
+      {
+        '<leader>cf',
+        function()
+          require('conform').format { async = true, lsp_format = 'fallback' }
+        end,
+        mode = '',
+        desc = 'Format buffer [Conform]',
+      },
+    },
     opts = {
-      event = { 'BufWritePre' }, -- lazy-load, but not for autoformatting
-      cmd = { 'ConformInfo' },
-      keys = {
-        -- Add a command to handle formatting
-        {
-          '<leader>cf',
-          function()
-            require('conform').format { async = true, lsp_format = 'fallback' }
-          end,
-          mode = '',
-          desc = 'Format buffer [Conform]',
-        },
+      format_on_save = {
+        timeout_ms = 200
       },
       formatters_by_ft = {
-        lua = { 'stylua' },
-        javascript = { 'prettier' },
-        typescript = { 'prettier' },
-        python = { 'black' },
-        rust = { 'rustfmt' },
+        -- Add language specific formatters here:
+        -- lua = { 'stylua' },
+        -- javascript = { 'prettier' },
+        -- typescript = { 'prettier' },
+        -- python = { 'black' },
+        -- rust = { 'rustfmt' },
+        ["_"] = { "trim_whitespace"},
       },
     },
   },
